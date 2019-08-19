d2680970d3d097ca1a16c4b530d665e153f6881e
Mikus Vanags <mikus.vanags@mittmedia.se>
2019-08-19 09:28:32 +0200

    implementing custom model behavior

---
+++ app/models/article.rb
@@ -0,0 +1,30 @@
+class Article < ApplicationRecord
+  attr_accessor :long_title
+  has_many :comments
+
+  before_validation :convert_title
+
+  # validates(:title, :body, {presence: true})
+  validates :title, :body, presence: true
+
+  def not_private
+    "JEP"
+  end
+
+  # self methods dont need class instance
+  def self.something
+    'hello'
+  end
+
+  private
+
+  def is_private
+    "NOPE"
+  end
+
+  def convert_title
+    return unless long_title.present?
+    self.title = long_title.truncate(10)
+  end
+
+end
